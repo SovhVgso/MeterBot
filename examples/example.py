2dc186f22a011f9a52477800ab61cd8460fb5d8e
SovhVgso <119291714+SovhVgso@users.noreply.github.com>
2024-11-03 23:34:23 +0800

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -6,19 +6,19 @@ event = Event(bot)
 #固定回复
 @event.on_message(r'测试')
 async def message_handler(event_structure):
-    event_structure.reply("测试")
+    await event_structure.reply("测试")
 
 #关键词检测
 @event.key_message(r'测试')
 async def message_handler(event_structure):
-    event_structure.reply("测试")
+    await event_structure.reply("测试")
 
 #使用正则表达式匹配
 @event.re_message(r'测试(.*)')
 async def message_handler(event_structure):
     data = event_structure.msgdata
-    event_structure.reply(data["match_msg"])#回复测试加后面的任意匹配内容
-    event_structure.reply(data["match_groups"])#回复所有被匹配到的组
+    await event_structure.reply(data["match_msg"])#回复测试加后面的任意匹配内容
+    await event_structure.reply(data["match_groups"])#回复所有被匹配到的组
 
 #延时回复
 @event.on_message(r'测试')
